87c41c26f9a062f8572256d7708f304cc637312e
GitHub Action <action@github.com>
2021-11-30 16:52:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/underkoen/adventofcodeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.underkoen.adventofcodeplugin.services
+
+import com.github.underkoen.adventofcodeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
