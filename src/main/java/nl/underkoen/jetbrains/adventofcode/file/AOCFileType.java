3ef466f05c2e0d94e257d3dc7f7898bdf4cacbd4
Koen van Staveren <koenvanstaveren@hotmail.com>
2021-12-10 08:48:43 +0100

    Added charset for IU-203

--- src/main/java/nl/underkoen/jetbrains/adventofcode/file/AOCFileType.java
+++ src/main/java/nl/underkoen/jetbrains/adventofcode/file/AOCFileType.java
@@ -4,6 +4,8 @@ import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.NlsSafe;
+import com.intellij.openapi.vfs.VirtualFile;
+import kotlin.text.Charsets;
 import nl.underkoen.jetbrains.adventofcode.general.components.Icons;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -37,5 +39,10 @@ public class AOCFileType implements FileType, DumbAware {
     @Override
     public boolean isBinary() {
         return false;
+    }
+
+    @Override
+    public @NonNls @Nullable String getCharset(@NotNull VirtualFile file, byte @NotNull [] content) {
+        return Charsets.UTF_8.name();
     }
 }
